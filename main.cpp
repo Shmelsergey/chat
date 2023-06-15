ec39038c15265c2d3e3845acac6c8e7927bccb2c
Shmelsergey <131392590+Shmelsergey@users.noreply.github.com>
2023-06-15 20:09:21 +0400

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,26 @@
+﻿// chat.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+
+#include "Chat.h"
+using namespace std;
+int main()
+{
+     Chat chat;
+     chat.start();
+     while (chat.isChatWork())
+     {
+         chat.showLoginMenu();
+
+         while (chat.getCurrentUser())
+         {
+             chat.showUserMenu();
+
+         }
+
+
+     }
+
+     // std::cout << "Hello World!\n";
+}
